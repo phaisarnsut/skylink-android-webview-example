5f8e7cd23199ef7e20ed53eb8c3f9c07a34eac3b
nitro2018 <phaisarn@outlook.com>
2019-10-24 16:10:56 +0700

    fix error by giving url to Webview

--- app/src/main/java/com/phaisarn/mywebview/MainActivity.java
+++ app/src/main/java/com/phaisarn/mywebview/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     private static final int REQUEST_AUDIO_PERMISSION = 1;
     private static final String[] PERM_AUDIO = {Manifest.permission.RECORD_AUDIO};
 
+    private static final String URL = "https://codepen.io/temasys/pen/GogabE";
+
     static String TAG = "Jackwebview";
 
     @Override
@@ -42,27 +44,32 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         myWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
 
         myWebView.setWebViewClient(new WebViewClient());
+
+        myWebView.loadUrl(URL);
+
         myWebView.setWebChromeClient(new WebChromeClient() {
             // Grant permissions for camera
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
                 Log.d(TAG, "onPermissionRequest");
 
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-                    @Override
-                    public void run() {
-                        if (request.getOrigin().toString().equals("*")) { // https://YOUR_ORIGIN_GOES_HERE
-                            request.grant(request.getResources());
-                        } else {
-                            request.deny();
-                        }
-                    }
-                });
+//                MainActivity.this.runOnUiThread(new Runnable() {
+//                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+//                    @Override
+//                    public void run() {
+//                        if (request.getOrigin().toString().equals("*")) { // https://YOUR_ORIGIN_GOES_HERE
+//                            request.grant(request.getResources());
+//                        } else {
+//                            request.deny();
+//                        }
+//                    }
+//                });
+//
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    request.grant(request.getResources());
+//                }
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    request.grant(request.getResources());
-                }
+                request.grant(request.getResources());
             }
 
             @Override
@@ -72,26 +79,38 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
             }
         });
 
-        if (EasyPermissions.hasPermissions(MainActivity.this, PERM_AUDIO) == false) {
-            EasyPermissions.requestPermissions(
-                    this,
-                    "This app needs access to your audio.",
-                    REQUEST_AUDIO_PERMISSION,
-                    PERM_AUDIO);
-        }
+//        if (EasyPermissions.hasPermissions(MainActivity.this, PERM_AUDIO) == false) {
+//            EasyPermissions.requestPermissions(
+//                    this,
+//                    "This app needs access to your audio.",
+//                    REQUEST_AUDIO_PERMISSION,
+//                    PERM_AUDIO);
+//        }
 
-        if (EasyPermissions.hasPermissions(MainActivity.this, PERM_CAMERA)) {
-            //myWebView.loadUrl("Your URL");
-            myWebView.loadUrl("https://codepen.io/temasys/pen/GogabE");
+//        if (EasyPermissions.hasPermissions(MainActivity.this, PERM_CAMERA)) {
+//            //myWebView.loadUrl("Your URL");
+//            myWebView.loadUrl(URL);
+//
+//            setContentView(myWebView);
+//        } else {
+//            EasyPermissions.requestPermissions(
+//                    this,
+//                    "This app needs access to your camera so you can take pictures.",
+//                    REQUEST_CAMERA_PERMISSION,
+//                    PERM_CAMERA);
+//        }
+    }
 
-            setContentView(myWebView);
-        } else {
-            EasyPermissions.requestPermissions(
-                    this,
-                    "This app needs access to your camera so you can take pictures.",
-                    REQUEST_CAMERA_PERMISSION,
-                    PERM_CAMERA);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myWebView.loadUrl(URL);
+
+        EasyPermissions.requestPermissions(
+                this,
+                "This app needs access to your audio.",
+                REQUEST_AUDIO_PERMISSION,
+                PERM_AUDIO);
     }
 
     @Override
@@ -105,6 +124,20 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     @Override
     public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
         Log.d(TAG, "onPermissionsGranted");
+
+        if (EasyPermissions.hasPermissions(MainActivity.this, PERM_CAMERA)) {
+            //myWebView.loadUrl("Your URL");
+            myWebView.loadUrl(URL);
+
+            setContentView(myWebView);
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs access to your camera so you can take pictures.",
+                    REQUEST_CAMERA_PERMISSION,
+                    PERM_CAMERA);
+        }
+
     }
 
     @Override
